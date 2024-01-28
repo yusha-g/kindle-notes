32f2e26b8c1a9f04e084256b4e842c11a77c930b
yusha-g <pratyusha.gh@gmail.com>
2024-01-28 16:11:10 +0530

    feat: extracting book title

--- exporter.py
+++ exporter.py
@@ -1,1 +1,11 @@
 
+my_clippings = open("resources/My Clippings.txt", "r")
+for line in my_clippings:
+    try:
+        # last character of every line is \n. So we take the second last character.
+        last_char = line[-2]
+    except IndexError:
+        # Empty line
+        continue
+    if last_char == ")":
+        print(line)
